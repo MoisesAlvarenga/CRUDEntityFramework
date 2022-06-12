0aa54a1b9bc6151604d2ae74fcbac06c307fe65b
Moises Alvarenga <moises.alvarenga@outlook.com>
2022-06-12 17:03:23 -0300

    Sistema de cadastor de produto e categorias

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace EstoqueWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
